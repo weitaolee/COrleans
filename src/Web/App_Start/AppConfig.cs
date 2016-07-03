c7cc107a7e535444af1be89acc8cf39c1320a93a
weitaolee <12666954@qq.com>
2016-07-03 16:54:22 +0800

    first commit

---
+++ src/Web/App_Start/AppConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Routing;
+using FC.Framework;
+using FC.Framework.Autofac;
+using FC.Framework.Log4net;
+
+namespace COrleans.Web
+{
+    public class AppConfig
+    {
+        public static void Initialize(RouteCollection routes)
+        {
+            FCFramework.Initialize()
+                       .UseAutofac()
+                       .UseLog4net()
+                       .UseDefaultJsonSerializer();
+        }
+    }
+}
---
+++ src/Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COrleans.Web.MvcApplication" Language="C#" %>
